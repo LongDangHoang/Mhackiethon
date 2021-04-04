a66b9576b4d1a46649b813f57dc3002d359297a3
pewwtato <steph.fong18@gmail.com>
2021-04-04 14:45:13 +1000

    Revert "Add files via upload"
    This reverts commit e917fb9269f02106795fb42ff9a09b0920c07708.

--- app.js
+++ app.js
@@ -1,52 +1,76 @@
-const progress = document.getElementById("progress");
-const prev = document.getElementById("prev");
-const next = document.getElementById("next");
-const circles = document.querySelectorAll(".circle");
+var express = require('express');
+// var firebase = require('firebase/app');
+// require('firebase/auth');
+var bodyParser = require('body-parser');
+var multer = require('multer'); // parse form-datea
+var path = require('path');
+const {joinUser, removeUser} = require('./user.js');
 
-let currentActive = 1;
+var app = express();
+var http = require('http').createServer(app);
+var io = require("socket.io")(http);
+// var upload = multer();
 
-next.addEventListener("click", ()=>{
-    currentActive++;
-    
-    if (currentActive > circles.length){
-        currentActive = circles.length;
-    }
-    update(); 
-});
+// firebase.initialzeApp({
+//     apiKey: "AIzaSyA5NhmYW7Q-ftU9hS5a6w9_TQDZXRNjMig",
+//     authDomain: "mhackiethon.firebaseapp.com",
+//     projectId: "mhackiethon",
+//     storageBucket: "mhackiethon.appspot.com",
+//     messagingSenderId: "1073593514181",
+//     appId: "1:1073593514181:web:b8283c5c1ddcb7f90a18ac"  
+// })
 
-// THIS IS FOR USING THE PREV BUTTON
-prev.addEventListener("click", ()=>{
-    currentActive--;
-    
-    if (currentActive < 1){
-        currentActive = 1;
-    }
-    update();
-});
+// const auth = firebase.auth();
 
-function update() {
-    circles.forEach((circle, idx)=>{
-        if (idx < currentActive) {
-            circle.classList.add("active");
-        } else{
-            circle.classList.remove("active");
-        }
+app.use(bodyParser.urlencoded());
+// app.use(upload.array());
+
+// set onClick of button to this for sign in
+// function signInWithGoogle() {
+//     const provider = new firebase.auth.GoogleAuthProvider();
+//     auth.signInWithPopup(provider);
+// }
+
+// function signOut() {
+//     auth.signOut();
+// }
+
+let thisRoom = "";
+io.on("connection", (socket) => {
+    console.log("connected");
+
+    // join room event
+    socket.on("join room", (data) => {
+        console.log("Recieved some info!");
+        console.log('in room');
+        let newUser = joinUser(socket.id, data.username, data.roomName);
+        socket.emit('send data', {
+            id: socket.id,
+            username: newUser.username,
+            roomName: newUser.roomName
+        })
+        thisRoom = newUser.roomName;
+        console.log(newUser);
+        socket.join(newUser.roomName);
     });
+});
 
-    const actives = document.querySelectorAll(".active");
+app.get('/', (req, res) => {
+    res.redirect('/login/');
+});
 
-    console.log(actives.length,circles.length);
-    progress.style.width = ((actives.length - 1) / (circles.length - 1))*100 + "%";
+app.use('/home/', express.static(path.join(__dirname, "static/homepage/")));
+app.use('/connect/', express.static(path.join(__dirname, "static/homepage/")));
 
-    if (currentActive === 1){
-        prev.disabled = true;
-    } else if (currentActive === circles.length) {
-        next.disabled = true;
-    } else {
-        prev.disabled = false;
-        next.disabled = false;
-    }
+app.get('/login/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/static/login/login.html'))
+});
 
-}
+app.post('/connect/', (req, res) => {
+    // create user with name and add them to room code
+    console.log(req.body);
+});
 
 
+
+http.listen(process.env.PORT || 3000);
